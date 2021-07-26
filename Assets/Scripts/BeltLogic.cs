d1b7f9c5d50bb030086396c2c58b4ffdd34a408f
Alex Lambert <hyperactive@live.com>
2021-07-26 15:06:45 -0700

    Updated BeltLogic to start looking for 'backBelt' from multiple sides and ensuring that backBelt actually points towards this belt

--- Assets/Scripts/BeltLogic.cs
+++ Assets/Scripts/BeltLogic.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 public class BeltLogic : Placeable
 {
 	GridControl grid;
+	// Reference to the Belt in front of this one
 	public BeltLogic frontBelt;
+	// Reference to the Belt in behind this one
+	// Note, only stores 1 belt that is pointing to this one
+	// Therefore there must be a priority system for determining which one it gets
 	public BeltLogic backBelt;
+	// Reference to the Item currently in this belt
 	public GameObject itemSlot;
 
 	public override void PlacedAction(GridControl grid_)
@@ -15,6 +20,7 @@ public class BeltLogic : Placeable
 		grid = grid_;
 		GameObject temp = null;
 
+		// If there is a belt in front of this one, connect them
 		if (grid.placeObjects.TryGetValue((transform.position + transform.right), out temp))
 		{
 			if (temp.GetComponent<BeltLogic>() == null)
@@ -27,21 +33,37 @@ public class BeltLogic : Placeable
 				frontBelt.backBelt = this;
 			}
 		}
-
+		
+		backBelt = null;
+		// If there is a belt directly behind this one and points to this, connect them
+		// Tries again for left, then right, if they don't attach
 		if (grid.placeObjects.TryGetValue((transform.position - transform.right), out temp))
 		{
-			if (temp.GetComponent<BeltLogic>() == null)
-			{
-				backBelt = null;
-			}
-			else
-			{
-				backBelt = temp.GetComponent<BeltLogic>();
-				backBelt.frontBelt = this;
-			}
+			TryToAttachBeltsHelper(temp);
+		}
+		if (backBelt == null && grid.placeObjects.TryGetValue((transform.position + transform.up), out temp))
+		{
+			TryToAttachBeltsHelper(temp);
+		}
+		if (backBelt == null && grid.placeObjects.TryGetValue((transform.position - transform.up), out temp))
+		{
+			TryToAttachBeltsHelper(temp);
 		}
 
 		grid.OnBeltTimerCycle += BeltCycle;
+	}
+
+	// Checks if the back belt exists and is pointing towards this one, then connects them
+	private void TryToAttachBeltsHelper(GameObject back)
+	{
+		if (back.GetComponent<BeltLogic>() == null)
+			return;
+		if (back.transform.position + back.transform.right == transform.position)
+		{
+			backBelt = back.GetComponent<BeltLogic>();
+			backBelt.frontBelt = this;
+			return;
+		}
 	}
 
 	public void BeltCycle(object sender, EventArgs e)
