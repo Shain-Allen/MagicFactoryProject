492dee9a4b439bd91866cb4491c7fa7e43ed8810
Alex Lambert <hyperactive@live.com>
2021-07-27 17:43:46 -0700

    Started working on vein spawning in OreGeneration.cs, still incomplete

--- Assets/Scripts/OreGeneration.cs
+++ Assets/Scripts/OreGeneration.cs
@@ -18,26 +18,31 @@ public class OreGeneration : MonoBehaviour
 
     private static void GenerateOres(GridControl grid, GameObject oreName, System.Random randGen, int leftEdge, int rightEdge, int topEdge, int bottomEdge)
     {
-        List<Vector3> oreSpawns = GetSpawnLocations(randGen, leftEdge, rightEdge, topEdge, bottomEdge);
+        List<Vector3Int> oreSpawns = GetSpawnLocations(randGen, leftEdge, rightEdge, topEdge, bottomEdge);
 
-        foreach(Vector3 oreCenter in oreSpawns)
+        foreach(Vector3Int oreCenter in oreSpawns)
             SpawnVein(grid, oreName, oreCenter, randGen, leftEdge, rightEdge, topEdge, bottomEdge);
     }
 
-    private static List<Vector3> GetSpawnLocations(System.Random randGen, int leftEdge, int rightEdge, int topEdge, int bottomEdge)
+    private static List<Vector3Int> GetSpawnLocations(System.Random randGen, int leftEdge, int rightEdge, int topEdge, int bottomEdge)
     {
-        List<Vector3> OreSpawns = new List<Vector3>();
+        List<Vector3Int> OreSpawns = new List<Vector3Int>();
 
         for (int x = leftEdge; x <= rightEdge; x++)
             for (int y = topEdge; y <= bottomEdge; y++)
                 if(randGen.NextDouble() <= SPAWN_CHANCE)
-                    OreSpawns.Add(new Vector3(x, y, 0));
+                    OreSpawns.Add(new Vector3Int(x, y, 0));
 
         return OreSpawns;
     }
 
-    private static void SpawnVein(GridControl grid, GameObject oreName, Vector3 center, System.Random randGen, int leftEdge, int rightEdge, int topEdge, int bottomEdge)
+    private static void SpawnVein(GridControl grid, GameObject oreName, Vector3Int center, System.Random randGen, int leftEdge, int rightEdge, int topEdge, int bottomEdge)
     {
-        grid.placeObjects.Add(center, Instantiate(oreName, center, Quaternion.identity, grid.transform));
+        for (int x = (center.x - MAX_RADIUS < leftEdge ? leftEdge : center.x - MAX_RADIUS); x <= (center.x + MAX_RADIUS > rightEdge ? rightEdge : center.x + MAX_RADIUS); x++)
+            for (int y = (center.y - MAX_RADIUS < topEdge ? topEdge : center.y - MAX_RADIUS); y <= (center.y + MAX_RADIUS > bottomEdge ? bottomEdge : center.y + MAX_RADIUS); y++)
+                if (Vector3Int.Distance(new Vector3Int(x, y, 0), center) < MAX_RADIUS)
+                {
+                    grid.placeObjects.Add((Vector2Int)center, Instantiate(oreName, center, Quaternion.identity, grid.transform));
+                }
     }
 }
