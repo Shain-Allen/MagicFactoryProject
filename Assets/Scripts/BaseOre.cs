4187cf5f26fd098ac91f3563d47d698798f95323
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-07-29 17:18:57 -0700

    made some updates in terms of Ore base class and OreDictinary object

---
+++ Assets/Scripts/BaseOre.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseOre
+{
+	public string oreType;
+	public GameObject outputItem;
+	public int remainingOre;
+}
