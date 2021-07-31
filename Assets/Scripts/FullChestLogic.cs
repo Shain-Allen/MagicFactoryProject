ae36304ae525b71ba8a128dbcd7243e86247d6ef
Alex Lambert <hyperactive@live.com>
2021-07-30 20:05:33 -0700

    Added Void and Full Chest with Sprites and empty scripts

---
+++ Assets/Scripts/FullChestLogic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullChestLogic : Placeable
+{
+    public override void PlacedAction(GridControl grid)
+    {
+
+    }
+
+	public override void RemovedAction()
+    {
+
+    }
+}
