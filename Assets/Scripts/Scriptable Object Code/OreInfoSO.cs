7d54f7b9db9a74e1fef84ae43f8b9c1cc61d254d
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-08-02 17:39:41 -0700

    made it so that as long as the OreInfo asset file name is the same as the Prefab name for the Ore then it will auto find the index

--- Assets/Scripts/Scriptable Object Code/OreInfoSO.cs
+++ Assets/Scripts/Scriptable Object Code/OreInfoSO.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "OreInfo", menuName = "OreInfoAsset")]
 public class OreInfoSO : ScriptableObject
@@ -7,9 +8,19 @@ public class OreInfoSO : ScriptableObject
 	public int DistanceMultiplyer = 1;
 	public int returnObjectIndex;
 	public ObjectDictionary itemDictionary;
+	public ObjectDictionary oreDictionary;
 
 	private void OnEnable()
 	{
 		itemDictionary = (ObjectDictionary)Resources.Load("Scriptable_Object_Items/ItemDictionary");
+		oreDictionary = (ObjectDictionary)Resources.Load("Scriptable_Object_Items/OreDictionary");
+
+		for (int i = 0; i < oreDictionary.itemList.Length; i++)
+		{
+			if (this.name == oreDictionary.itemList[i].name)
+			{
+				returnObjectIndex = i;
+			}
+		}
 	}
 }
