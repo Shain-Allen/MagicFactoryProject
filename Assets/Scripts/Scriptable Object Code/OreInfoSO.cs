fd647dd1ba775a24e3e839fed2beb539bfcaa9ce
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-08-02 17:51:34 -0700

    added ToolTips to OreInfo scriptable objects

--- Assets/Scripts/Scriptable Object Code/OreInfoSO.cs
+++ Assets/Scripts/Scriptable Object Code/OreInfoSO.cs
@@ -4,16 +4,21 @@ using UnityEditor;
 [CreateAssetMenu(fileName = "OreInfo", menuName = "OreInfoAsset")]
 public class OreInfoSO : ScriptableObject
 {
+	[Min(1), Tooltip("the minimum amount of ore that can ever spawn even when right at 0,0 in the world")]
 	public int baseOreAmount = 1;
-	public int DistanceMultiplyer = 1;
+	[Min(1), Tooltip("how quickly the amount of avalable ore will scale with distance from spawn")]
+	public float DistanceMultiplier = 1;
+
+	[Header("Autofilled fields (or at least they should be")]
+	[Tooltip("this is where in the ItemDictionary to find the Item Version of this ore")]
 	public int returnObjectIndex;
+	[Tooltip("reference to the Dictionary asset that stores all the items in the game")]
 	public ObjectDictionary itemDictionary;
-	public ObjectDictionary oreDictionary;
 
 	private void OnEnable()
 	{
 		itemDictionary = (ObjectDictionary)Resources.Load("Scriptable_Object_Items/ItemDictionary");
-		oreDictionary = (ObjectDictionary)Resources.Load("Scriptable_Object_Items/OreDictionary");
+		ObjectDictionary oreDictionary = (ObjectDictionary)Resources.Load("Scriptable_Object_Items/OreDictionary");
 
 		for (int i = 0; i < oreDictionary.itemList.Length; i++)
 		{
