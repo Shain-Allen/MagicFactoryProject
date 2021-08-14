b3df5c37be169d88ca4631f71fe14f2dfe91bab1
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-08-13 17:28:41 -0700

    added Tick system

--- Assets/Scripts/World Logic/GridControl.cs
+++ Assets/Scripts/World Logic/GridControl.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class GridControl : MonoBehaviour
 {
 	public event EventHandler OnBeltTimerCycle;
+	public static event EventHandler Tick;
 	// Vector2Int = Chunk coordinates, and GameObject is an empty object to be the parent of all ores in that chunk
 	public Dictionary<Vector2Int, GameObject> worldChunks = new Dictionary<Vector2Int, GameObject>();
 
 	public float beltCycleTime { get; } = 2f;
 	float beltCycleTimeLeft = 0f;
 	public int worldSeed = 0;
+
+	[SerializeField]
+	private int tps = 10;
+	private float tickRate;
+	private float tickTimer = 0;
 
 	public List<GameObject> oreNames;
 	public List<GameObject> oreOutputItems;
@@ -19,6 +25,8 @@ public class GridControl : MonoBehaviour
 	private void Start()
 	{
 		beltCycleTimeLeft = beltCycleTime;
+
+		tickRate = 1f / tps;
 
 		int initialChunkSpawningRadius = 0;
 		for (int x = -initialChunkSpawningRadius; x <= initialChunkSpawningRadius; x++)
@@ -33,12 +41,19 @@ public class GridControl : MonoBehaviour
 	private void Update()
 	{
 		beltCycleTimeLeft -= Time.deltaTime;
+		tickTimer += Time.deltaTime;
 
 		if (beltCycleTimeLeft <= 0)
 		{
 			//Debug.Log("Belt pulse");
 			beltCycleTimeLeft = beltCycleTime;
 			OnBeltTimerCycle?.Invoke(this, EventArgs.Empty);
+		}
+
+		if (tickTimer >= tickRate)
+		{
+			tickTimer = 0;
+			Tick?.Invoke(this, EventArgs.Empty);
 		}
 	}
 }
