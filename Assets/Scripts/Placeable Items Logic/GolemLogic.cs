6e4af346d840d12cfa6dd2ce8acbd15d51e7914f
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-08-18 17:03:58 -0700

    Started to implement Golem

---
+++ Assets/Scripts/Placeable Items Logic/GolemLogic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemLogic : Placeable
+{
+	public override void PlacedAction(GridControl grid_)
+	{
+
+	}
+
+	public override void RemovedAction()
+	{
+
+	}
+}
